78baa7a3ba2a3440086c47993550b0f60c659709
fripig <fripig@gmail.com>
2018-05-07 16:44:36 +0800

    add close button

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="card" style="width: 18rem;">
+
   <div class="card-body">
     <h5 class="card-title">請用這個愛心碼</h5>
+    <button type="button"
+            class="close"
+            data-dismiss="modal"
+            aria-label="Close"
+            @click="removeCard(card.uuid)">
+      <span aria-hidden="true">&times;</span>
+    </button>
     <h5 class="card-title" v-show="card.notableware" >不用餐具</h5>
     <h6 class="card-subtitle mb-2 text-muted">{{card.shortname}}</h6>
     <h6 class="card-subtitle mb-2 text-muted">{{card.code}}</h6>
@@ -10,8 +18,12 @@
 </div>
 </template>
 <script>
+    import { mapGetters, mapActions } from "vuex";
 export default {
-    props:['card']
+    props:['card'],
+    methods: {
+        ...mapActions(['removeCard'])
+    },
 }
 </script>
 
