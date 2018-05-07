daae25032f5ec9ef5317169050a7062bf621c765
fripig <fripig@gmail.com>
2018-05-07 17:23:03 +0800

    remove close button

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,14 +1,8 @@
 <template>
-  <div class="card" style="width: 18rem;">
-
+  <div class="card" style="width: 18rem;" @click="removeCard(card.uuid)">
   <div class="card-body">
     <h5 class="card-title">
         請用這個愛心碼
-        <button type="button"
-                class="close"
-                @click="removeCard(card.uuid)">
-            <span aria-hidden="true">&times;</span>
-        </button>
     </h5>
 
     <h5 class="card-subtitle mb-2 text-muted">{{card.shortname}}</h5>
